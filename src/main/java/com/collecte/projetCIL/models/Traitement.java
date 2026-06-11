971ddecb837c3ed491151fec1f75ec13debf01ea
Nacanabo elvis <nacanaboelvis7@gmail.com>
2026-06-11 13:35:56 +0200

    correction de bug au lencement

--- src/main/java/com/collecte/projetCIL/models/Traitement.java
+++ src/main/java/com/collecte/projetCIL/models/Traitement.java
@@ -15,6 +15,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.collecte.projetCIL.enums.StatutTraitement;
 
 @Entity
 @Table(name = "traitement")
